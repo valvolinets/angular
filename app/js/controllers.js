855ae70a6fc187b636e9dc021e3df7330b28daee
Valerijs Volinecs <Val@Valerijss-MacBook-Air.local>
2017-05-11 23:49:49 +0300

    Initial commit. Hands on directives and filters.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,33 @@
 'use strict';
 
 /* Controllers */
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+$scope.phones = [
+{'name': 'Nexus', 'desc': 'string 1.', 'status': true},
+
+{'name': 'Galaxy S8', 'desc': 'Lorem ipsum .', 'status': false},
+
+{'name': 'Iphone 7', 'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'status': true}
+];
+
+
+//Filters
+var date = new Date();
+$scope.today = date;
+
+$scope.sortField = undefined;
+$scope.reverse = false;
+
+$scope.sort = function(fieldName) {
+    if($scope.sortField === fieldName) {
+        $scope.reverse = !$scope.reverse;
+    } else {
+        $scope.sortField = fieldName;
+            $scope.reverse = false;
+    }
+}
+
+});
+
